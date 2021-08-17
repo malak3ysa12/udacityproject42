dcf8df3ec6619e5fc71f144f515db6727d875ecf
Malak Essa <53011237+malak3ysa12@users.noreply.github.com>
2021-08-17 12:53:40 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=malak3ysa/udacitiproject04
+dockerpath=malak3ysa/udacitiproject42
 
 # Step 2
 # Run the Docker Hub container with kubernetes
